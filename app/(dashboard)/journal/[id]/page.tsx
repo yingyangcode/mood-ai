cc334211e91c04caa0337e2d49474c74899bfaf2
yingyangcode <marcel.anis.21@gmail.com>
2023-07-07 00:50:01 -0400

    - Added editor component to entry page

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,29 @@
-const EntryPage = ({ params }) => {
-  return <div>{params.id}</div>
+import Editor from '@/components/Editor'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id) => {
+  const user = await getUserByClerkID()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      id,
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
+
+const EntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div className="w-full h-full">
+      <Editor entry={entry} />
+    </div>
+  )
 }
 
 export default EntryPage
