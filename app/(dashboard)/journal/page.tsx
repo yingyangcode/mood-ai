bed7f40c27acacef1c4636d9e75edad04cde940a
yingyangcode <marcel.anis.21@gmail.com>
2023-07-07 00:16:58 -0400

    - Made entry card clickable to navigate to entry page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,7 @@ import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 
 const getEntries = async () => {
   const user = await getUserByClerkID()
@@ -19,12 +20,14 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries()
   return (
-    <div className="p-10 bg-zing-400/10 h-full">
+    <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
       <div className="grid grid-cols-3 gap-4 ">
         <NewEntryCard />
         {entries.map((entry) => (
-          <EntryCard key={entry.id} entry={entry} />
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard entry={entry} />
+          </Link>
         ))}
       </div>
     </div>
