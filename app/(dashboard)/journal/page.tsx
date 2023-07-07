620c41a0456ea27bf8de6a148bc24dc34aedd644
yingyangcode <marcel.anis.21@gmail.com>
2023-07-06 23:40:09 -0400

    - Added journal page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,3 +1,5 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
@@ -16,7 +18,17 @@ const getEntries = async () => {
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  return <div>journal</div>
+  return (
+    <div className="p-10 bg-zing-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4 ">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage
---
+++ components/EntryCard.tsx
@@ -0,0 +1,5 @@
+export const EntryCard = ({ entry }) => {
+  return <div>{entry.id}</div>
+}
+
+export default EntryCard
