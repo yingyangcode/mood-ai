58a2054c6f468de0f9e46e890be5a3b91ac2bfcc
yingyangcode <marcel.anis.21@gmail.com>
2023-07-10 23:49:05 -0400

    - Update analysis when updating entry in api routes

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,5 +1,7 @@
+import { analyze } from "@/utils/ai"
 import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import { revalidatePath } from "next/cache"
 import { NextResponse } from "next/server"
 
 export const PATCH = async (request: Request, {params}) => {
@@ -18,5 +20,18 @@ export const PATCH = async (request: Request, {params}) => {
         },
     })
 
-    return NextResponse.json({data: updatedEntry})
+    const analysis = await analyze(updatedEntry.content)
+    const updated = await prisma.analysis.upsert({
+        where: {
+            entryId: updatedEntry.id,
+        },
+        create: {
+            entryId: updatedEntry.id,
+            ...analysis
+        },
+        update: analysis,
+    })
+
+
+    return NextResponse.json({data: {...updatedEntry, analysis: updated}})
 }
