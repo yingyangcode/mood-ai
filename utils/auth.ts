ba50b3ad48cf924bc1676a0819079c04ff4c6d1e
yingyangcode <marcel.anis.21@gmail.com>
2023-07-11 01:37:09 -0400

    - Added selection criteria when getting user by clerkId in db

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,14 +1,42 @@
+import type {User} from '@clerk/nextjs/api'
 import { auth } from "@clerk/nextjs"
 import { prisma } from "./db"
 
-export const getUserByClerkID = async () => {
+export const getUserByClerkID = async (select = {id:true}) => {
     const {userId} = await auth()
 
     const user = await prisma.user.findUniqueOrThrow({
         where: {
-            clerkId: userId,
+            clerkId: userId as string,
         },
+        select,
     })
 
     return user
 }
+
+export const syncNewUser = async (clerkUser: User) => {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: clerkUser.id,
+      },
+    })
+  
+    if (!existingUser) {
+      const email = clerkUser.emailAddresses[0].emailAddress
+      // const { subscriptionId, customerId } = await createAndSubNewCustomer(email)
+  
+      await prisma.user.create({
+        data: {
+          clerkId: clerkUser.id,
+          email,
+          account: {
+            create: {
+              // stripeCustomerId: customerId,
+              // stripeSubscriptionId: subscriptionId,
+            },
+          },
+        },
+      })
+    }
+  }
